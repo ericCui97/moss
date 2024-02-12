187d03afea07f59239fa425448e942a2596db7e2
cuiyirong <eric_cyr@163.com>
2024-02-12 21:21:59 +0800

    cmoss: compiler lex

---
+++ cmoss/compiler.h
@@ -0,0 +1,4 @@
+#ifndef __CMOSS_COMPILER_H__
+#define __CMOSS_COMPILER_H__
+void compile(const char* source);
+#endif
--- cmoss/vm.c
+++ cmoss/vm.c
@@ -2,6 +2,7 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "compiler.h"
 
 VM          vm;
 static void resetStack()
@@ -78,12 +79,11 @@ static InterpretResult run()
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk)
+InterpretResult interpret(const char* source)
 {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    //   return run();
-    return run();
+    compile(source);
+  return INTERPRET_OK;
+
 }
 
 void push(Value value)
