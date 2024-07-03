d4bf185c69cc7854239fb9fcc61f0cabf8d4af45
cuiyirong <eric_cyr@163.com>
2024-07-03 14:53:46 +0800

    解决段错误

--- cmoss/main.c
+++ cmoss/main.c
@@ -1,13 +1,14 @@
+#include <stdlib.h>
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
 
 int main(int argc, char** argv)
 {
-    Chunk* chunk;
-    init_chunk(chunk);
-    write_chunk(chunk, OP_RETURN);
-    disassemble_chunk(chunk, "test chunk");
-    free_chunk(chunk);
+    Chunk chunk;
+    init_chunk(&chunk);
+    write_chunk(&chunk, OP_RETURN);
+    disassemble_chunk(&chunk, "test chunk");
+    free_chunk(&chunk);
     return 0;
 }
